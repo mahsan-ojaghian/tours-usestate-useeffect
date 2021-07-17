c99d2340f70ef8822d5df6ff1ee6dfc52a524768
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-07-18 02:02:37 +0430

    DONE

--- src/Loading.js
+++ src/Loading.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="loading">
-      
-
+    <div className="loading">  
 <div className="loading">Loading&#8230;</div>
-
-
     </div>
   );
 };
